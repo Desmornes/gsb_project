05d6dd281fe07040c8ef99ae904a230baf470a12
cnaelle <naelle.kmaret@gmail.com>
2024-03-14 10:12:38 +0100

    fix return

--- modele_gsb/modele_gsb.php
+++ modele_gsb/modele_gsb.php
@@ -14,25 +14,25 @@ function getMedicaments()
     }
     $result->closeCursor();
     header('Content-Type: application/json');
-    echo json_encode($response, JSON_PRETTY_PRINT);
+    return $response;
 }
 // requete qui recupere un enregistrement de la table medicament et le renvoie en json au client
 function getMedicament($id=0)
 {
-   global $conn;
-   $query = "SELECT * FROM medicament";
-   if($id !=0)
-   {
-    $query .= " WHERE id=".$id."LIMIT 1";
-   } 
-  $conn-> query("SET NAMES utf8");
-  $result = $conn->query($query);
-  while($row = $result->fetch())
-  {
-    $response[]= $row;
-  }
-  header('Content-Type: application/json');
-    echo json_encode($response, JSON_PRETTY_PRINT);
+    global $conn;
+    $query = "SELECT * FROM medicament";
+    if($id !=0)
+    {
+        $query .= " WHERE id=".$id."LIMIT 1";
+    } 
+    $conn-> query("SET NAMES utf8");
+    $result = $conn->query($query);
+    while($row = $result->fetch())
+    {
+        $response[]= $row;
+    }
+    header('Content-Type: application/json');
+    return $response;
 }
 // requete qui recupere tout les enregistrements de la table effet_therapeutique
 function getEffetsTherapeutiques()
@@ -49,25 +49,25 @@ function getEffetsTherapeutiques()
     }
     $result->closeCursor();
     header('Content-Type:application/json');
-    echo json_encode($response, JSON_PRETTY_PRINT);
+    return $response;
 }
 // requete qui recupere un enregistrement de la table medicament et le renvoie en json au client
 function getEffetTherapeutique($id=0)
 {
     global $conn;
-   $query = "SELECT * FROM effets_therapeutiques";
-   if($id !=0)
-   {
-    $query .= " WHERE id=".$id."LIMIT 1";
-   } 
-  $conn-> query("SET NAMES utf8");
-  $result = $conn->query($query);
-  while($row = $result->fetch())
-  {
-    $response[]= $row;
-  }
-  header('Content-Type: application/json');
-    echo json_encode($response, JSON_PRETTY_PRINT);
+    $query = "SELECT * FROM effets_therapeutiques";
+    if($id !=0)
+    {
+        $query .= " WHERE id=".$id."LIMIT 1";
+    } 
+    $conn-> query("SET NAMES utf8");
+    $result = $conn->query($query);
+    while($row = $result->fetch())
+    {
+        $response[]= $row;
+    }
+    header('Content-Type: application/json');
+    return $response;
  
 }
 
@@ -86,25 +86,25 @@ function getEffetsSecondaires()
     }
     $result-> closeCursor();
     header('Content-Type:application/json');
-    echo json_encode($response, JSON_PRETTY_PRINT);
+    return $response;
 }
 // requete qui recupere un enregistrement de la table effets_secondaires et le renvoie au client
 function getEffetSecondaire($id=0)
 {
     global $conn;
-   $query = "SELECT * FROM effets_secondaires";
-   if($id !=0)
-   {
-    $query .= " WHERE id=".$id."LIMIT 1";
-   } 
-  $conn-> query("SET NAMES utf8");
-  $result = $conn->query($query);
-  while($row = $result->fetch())
-  {
-    $response[]= $row;
-  }
-  header('Content-Type: application/json');
-    echo json_encode($response, JSON_PRETTY_PRINT);  
+    $query = "SELECT * FROM effets_secondaires";
+    if($id !=0)
+    {
+        $query .= " WHERE id=".$id."LIMIT 1";
+    } 
+    $conn-> query("SET NAMES utf8");
+    $result = $conn->query($query);
+    while($row = $result->fetch())
+    {
+        $response[]= $row;
+    }
+    header('Content-Type: application/json');
+    return $response; 
 }
 
     ?>
