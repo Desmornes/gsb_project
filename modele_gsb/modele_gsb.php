5df0f3371f5978b5acba31ee1c8698b6fd36da37
Nephtalie Desmornes <137033061+Desmornes@users.noreply.github.com>
2024-03-14 09:52:19 +0100

    Update modele_gsb.php

--- modele_gsb/modele_gsb.php
+++ modele_gsb/modele_gsb.php
@@ -1,5 +1,5 @@
-<?php 
-
+<?php
+// requete qui recupere tout les enregistrements de la table medicament
 function getMedicaments()
     {
         global $conn;
@@ -14,8 +14,97 @@ function getMedicaments()
     }
     $result->closeCursor();
     header('Content-Type: application/json');
-    return $response;
+    echo json_encode($response, JSON_PRETTY_PRINT);
+}
+// requete qui recupere un enregistrement de la table medicament et le renvoie en json au client
+function getMedicament($id=0)
+{
+   global $conn;
+   $query = "SELECT * FROM medicament";
+   if($id !=0)
+   {
+    $query .= " WHERE id=".$id."LIMIT 1";
+   } 
+  $conn-> query("SET NAMES utf8");
+  $result = $conn->query($query);
+  while($row = $result->fetch())
+  {
+    $response[]= $row;
+  }
+  header('Content-Type: application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+}
+// requete qui recupere tout les enregistrements de la table effet_therapeutique
+function getEffetsTherapeutiques()
+{
+    global $conn;
+    $query = "SELECT * FROM effets_therapeutiques";
+    $response = array();
 
+    $conn->query("SET NAMES utf8");
+    $result = $conn->query($query);
+    while($row =$result->fetch())
+    {
+        $response[]= $row;
     }
+    $result->closeCursor();
+    header('Content-Type:application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+}
+// requete qui recupere un enregistrement de la table medicament et le renvoie en json au client
+function getEffetTherapeutique($id=0)
+{
+    global $conn;
+   $query = "SELECT * FROM effets_therapeutiques";
+   if($id !=0)
+   {
+    $query .= " WHERE id=".$id."LIMIT 1";
+   } 
+  $conn-> query("SET NAMES utf8");
+  $result = $conn->query($query);
+  while($row = $result->fetch())
+  {
+    $response[]= $row;
+  }
+  header('Content-Type: application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+ 
+}
 
-?>
+// requete qui recupere tout les enregistrements de la table effef_secondaire
+function getEffetsSecondaires()
+{
+    global $conn;
+    $query = "SELECT * FROM effets_secondaires";
+    $response = array();
+
+    $conn->query("SET NAMES utf8");
+    $result = $conn->query($query);
+    while ($row =$result->fetch())
+    {
+        $response[] = $row;
+    }
+    $result-> closeCursor();
+    header('Content-Type:application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+}
+// requete qui recupere un enregistrement de la table effets_secondaires et le renvoie au client
+function getEffetSecondaire($id=0)
+{
+    global $conn;
+   $query = "SELECT * FROM effets_secondaires";
+   if($id !=0)
+   {
+    $query .= " WHERE id=".$id."LIMIT 1";
+   } 
+  $conn-> query("SET NAMES utf8");
+  $result = $conn->query($query);
+  while($row = $result->fetch())
+  {
+    $response[]= $row;
+  }
+  header('Content-Type: application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);  
+}
+
+    ?>
